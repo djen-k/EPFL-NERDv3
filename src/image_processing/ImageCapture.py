Fixed strain detection reference problem. Exclude outliers/glitched images when averaging. Small setup dialog fixes. Increased threshold and timeout of switchboard waiting to reach set voltage to avoid oversensitive fault detection.

src/image_processing/ImageCapture.py
@@ -1,7 +1,7 @@
 import logging
 import time
 from datetime import datetime
-from threading import Thread, Event, Lock, Timer
+from threading import Thread, Event, Lock
 
 import cv2 as cv
 import numpy as np
@@ -428,7 +428,12 @@ class ImageCapture:
 
         return frames
 
-    def read_single(self, cam_id):
+    def read_single_image(self, cam_id):
+        """
+        Read a single image from the specified camera
+        :param cam_id: A camera identifier which can be either a camera name string or an index
+        :return: a newly retrieved image from the specified camera
+        """
         # check if is initialized
         if self._camera_count == -1:  # not initialized yet  --> go find cameras
             self.logging.info("ImageCapture not yet initialized. Initializing cameras...")
@@ -443,29 +448,62 @@ class ImageCapture:
 
         return None  # in case anything failed
 
-    def read_average(self, n):
+    def read_average_images(self, n, deviation_threshold=0.03):
         """
-        Read several images and average them (for each camera)
-        :param n: How many images to average
+        Read several images and average them (for each camera) to get a set of images with reduced noise.
+        If any image in the set of images to be averaged deviates too much from the others, it is discarded and a new image
+        is recorded until the desired number of images is available for averaging.
+        :param int n: How many images to average
+        :param float deviation_threshold: Image deviation threshold above which an image is excluded from the average,
+        given as a value between 0 and 1 (e.g. 0.1 -> image is discarded if deviation if more than 10%)
         :return: A set of averaged images
         """
-        image_sets = [self.read_images() for i in range(n)]
-        image_sets_np = np.array(image_sets)
-        if image_sets_np.ndim == 5:
-            # all images are the same size and have been combined into a numeric array
-            # of size [n_sets x n_cams x width x height x c_depth]  -> ndims == 5
-            avgs = np.mean(image_sets_np, axis=0).round().astype(np.uint8)
-            avgs = list(avgs)
-        elif image_sets_np.ndim == 2:  # images are of different size and could not be combined into a numeric array
-            avgs = []
-            for i in range(len(image_sets[0])):  # iterate over number of cams
-                img_set = [sets[i] for sets in image_sets]
-                img_set = np.array(img_set)
-                avg = np.mean(img_set, axis=0).round().astype(np.uint8)
-                avgs.append(avg)
-        else:
-            self.logging.critical("Image sets for averaging have unexpected shape...?!")
-            raise Exception("Image sets for averaging have unexpected shape...?!")
+        # get set of n images from each camera
+        image_sets = []
+        for i in range(n):
+            img_set = self.read_images()
+            # check for outliers (glitched images)
+            if i > 0:  # nothing to compare with just the first image
+                # check for new image from each camera
+                for c in range(self._camera_count):
+                    deviation = StrainDetection.get_image_deviation(image_sets[0][c], img_set[c])
+                    new_image_count = 0
+                    new_reference_count = 0
+                    while deviation > deviation_threshold:
+                        if new_image_count > 5:  # make sure we don't get stuck trying indefinitely
+                            if new_reference_count > 0:  # make sure we don't get stuck trying indefinitely
+                                self.logging.debug("Failed to record matching image for new reference "
+                                                   "for camera {}. Aborting.".format(c))
+                                img_set[c] = None
+                                break
+                            self.logging.debug("Failed to record matching images for averaging "
+                                               "for camera {}. Using new reference".format(c))
+                            image_sets[0][c] = self.read_single_image(c)
+                            new_reference_count = + 1
+                            new_image_count = 0  # reset count
+                        else:
+                            self.logging.debug("Image averaging: Outlier detected "
+                                               "in image {} from camera {}. Recording new image.".format(i, c))
+                        img_set[c] = self.read_single_image(c)
+                        deviation = StrainDetection.get_image_deviation(image_sets[0][c], img_set[c])
+                        new_image_count += 1
+            image_sets.append(img_set)
+
+        # transpose so we have one list of n images for each camera (instead of n lists of one image per camera)
+        image_sets = list(map(list, zip(*image_sets)))
+
+        avgs = []
+        for i in range(self._camera_count):  # iterate over number of cams. there must be one set for each cam
+            img_set = image_sets[i]
+            # remove Nones (where not matching image could be recorded even after trying real hard)
+            img_set = [img for img in img_set if img is not None]
+            if len(img_set) < n:
+                msg = "Only {} matching images from {} could be recorded for averaging (instead of {})."
+                self.logging.warning(msg.format(len(img_set), self._camera_names[i], n))
+            img_set = np.array(img_set)
+            avg = np.mean(img_set, axis=0).round().astype(np.uint8)
+            avgs.append(avg)
+
         return avgs
 
     def get_images_from_buffer(self):
@@ -669,42 +707,46 @@ if __name__ == '__main__':
 
     cap = SharedInstance
     cap.find_cameras()
-    cap.select_cameras([0])
-
-    # print("exposure:", cap.set_fixed_exposure())
-
-    times = []
-    frames = []
-    lock = Lock()
-    timestamp_start = datetime.now()  # record reference time
-
-
-    def new_frame(image, timestamp, camera_id):
-        with lock:
-            frames.append(image)
-            times.append((timestamp - timestamp_start).total_seconds())
-            print("received frame {}".format(len(frames)))
-
-
-    cap.set_new_image_callback(new_frame)
-    cap.start_capture_thread(max_fps=0)
-    timer = Timer(10, cap.stop_capture_thread)
-    timer.start()
-
-    frame_index = 0
-    time.sleep(11)
-    # while cap.is_capture_thread_running():
+    _avg = cap.read_average_images(10)
+    for _img in _avg:
+        cv.imshow("Image", _img)
+        cv.waitKey()
+    # cap.select_cameras([0])
+    #
+    # # print("exposure:", cap.set_fixed_exposure())
+    #
+    # times = []
+    # frames = []
+    # lock = Lock()
+    # timestamp_start = datetime.now()  # record reference time
+    #
+    #
+    # def new_frame(image, timestamp, camera_id):
     #     with lock:
-    #         if len(frames) > frame_index:
-    #             cv.imshow("Current Frame", frames[-1])
-    #             cv.waitKey(1)  # refresh window and check for user input
-    #             frame_index = len(frames)
-
-    elapsed_time_s = (datetime.now() - timestamp_start).total_seconds()
-    cap.close_cameras()
-
-    print("elapsed time: {} s".format(elapsed_time_s))
-    np.set_printoptions(formatter={'float': '{: 0.2f}'.format}, linewidth=500)  # set print format for arrays
-    print(np.array(times).reshape((-1, 1)))
-    time.sleep(1)
-    logging.info("done")
+    #         frames.append(image)
+    #         times.append((timestamp - timestamp_start).total_seconds())
+    #         print("received frame {}".format(len(frames)))
+    #
+    #
+    # cap.set_new_image_callback(new_frame)
+    # cap.start_capture_thread(max_fps=0)
+    # timer = Timer(10, cap.stop_capture_thread)
+    # timer.start()
+    #
+    # frame_index = 0
+    # time.sleep(11)
+    # # while cap.is_capture_thread_running():
+    # #     with lock:
+    # #         if len(frames) > frame_index:
+    # #             cv.imshow("Current Frame", frames[-1])
+    # #             cv.waitKey(1)  # refresh window and check for user input
+    # #             frame_index = len(frames)
+    #
+    # elapsed_time_s = (datetime.now() - timestamp_start).total_seconds()
+    # cap.close_cameras()
+    #
+    # print("elapsed time: {} s".format(elapsed_time_s))
+    # np.set_printoptions(formatter={'float': '{: 0.2f}'.format}, linewidth=500)  # set print format for arrays
+    # print(np.array(times).reshape((-1, 1)))
+    # time.sleep(1)
+    # logging.info("done")

src/image_processing/StrainDetection.py
@@ -22,10 +22,10 @@ class StrainDetector:
         self.setting_reference = False
 
         self.query_angles = np.array(query_angles)
-        self._strain_threshold = 50  # above 50 % strain can be considered an outlier
-        self._neg_strain_threshold = -10  # below -10 % strain can be considered an outlier
-        self._shift_threshold = 100  # above 100 px shift along either axis can be considered an outlier
-        self._image_deviation_threshold = 0.15  # if the average deviation is more than 15%, something bad happened
+        self.outlier_strain_threshold = 50  # above 50 % strain can be considered an outlier
+        self.outlier_negative_strain_threshold = -10  # below -10 % strain can be considered an outlier
+        self.center_shift_threshold = 100  # above 100 px shift along either axis can be considered an outlier
+        self.image_deviation_threshold = 0.1  # if the average deviation is more than 10%, something bad happened
 
     def set_reference(self, reference_images):
         self._reference_images = None
@@ -82,7 +82,7 @@ class StrainDetector:
             if self._reference_images is not None:
                 img_dev = self.get_deviation_from_reference(imgs)
                 self.logging.debug("Image deviations: {}".format(img_dev))
-                outlier = np.bitwise_or(outlier, np.array(img_dev) > self._image_deviation_threshold)
+                outlier = np.bitwise_or(outlier, np.array(img_dev) > self.image_deviation_threshold)
 
         # initialize lists to store results
         ellipses = [None] * n_img
@@ -92,7 +92,17 @@ class StrainDetector:
 
         # get fit for each DEA
         for i in range(n_img):
-            ellipse, mask = dea_fit_ellipse(imgs[i], self._exclude_masks[i])
+            ellipse, mask = dea_fit_ellipse(imgs[i], masks[i])
+            if masks[i] is None:
+                # If there was no mask on the first run, the first run is only used to generate a mask.
+                # We then run fit ellipse again to generate the actual fit.
+                # This step is crucial to ensure that subsequent calls to dea_fit_ellipse return consistent results.
+                # The values returned without a mask differ from those with a mask.
+                # Should maybe fix this at some point but for now we'll just call fit ellipse twice.
+                self.logging.debug("ellipse fitting was run without exclusion masks. "
+                                   "This can lead to inaccurate results. "
+                                   "Running ellipse fit again with newly generated masks.")
+                ellipse, mask = dea_fit_ellipse(imgs[i], mask)
             ellipses[i] = ellipse
             masks[i] = mask
 
@@ -134,11 +144,13 @@ class StrainDetector:
 
         # check if strain is too large
         if check_visual_state:
-            strain_out = np.bitwise_or(strain > self._strain_threshold, strain < self._neg_strain_threshold)
+            strain = strain * 100 - 100  # convert to engineering strain
+            strain_out = np.bitwise_or(strain > self.outlier_strain_threshold,
+                                       strain < self.outlier_negative_strain_threshold)
             strain_out = np.any(strain_out, axis=1)
             outlier = np.bitwise_or(outlier, strain_out)
             # TODO: make it so this doesn't crash if value are None or nan
-            shift_out = np.abs(center_shift) > self._shift_threshold
+            shift_out = np.abs(center_shift) > self.center_shift_threshold
             shift_out = np.any(shift_out, axis=1)
             outlier = np.bitwise_or(outlier, shift_out)
 
@@ -340,7 +352,6 @@ def find_electrode_outline(img_bw, exclude_mask=None, max_iterations=20, center=
     # get contours
     cont = find_contour(img_bw, center)
     ellipse = None
-    old_area = np.nan
     new_area = np.nan
     converged = False
     i = 0
@@ -718,13 +729,13 @@ if __name__ == '__main__':
 
     duallog.setup("logs", minlevelConsole=logging.DEBUG, minLevelFile=logging.DEBUG)
 
-    # test_image_folder = "D:/NERD output/NERD test 20200225-182457/DEA 1/Images/"
-    test_image_folder = "../../output/Test7/"
+    test_image_folder = "C:/Users/Djen/PycharmProjects/EPFL-NERDv3/output/" \
+                        "NERD test 20200515-112322 test strain detection on this one!/DEA 2/Images/"
+    # test_image_folder = "../../output/Test7/"
     files = os.listdir(test_image_folder)
 
     _strain_detector = StrainDetector((0, 90))
 
-    tic = time.perf_counter()
     ref_img = None
     for file in files:
         fpath = os.path.join(test_image_folder, file)
@@ -734,16 +745,22 @@ if __name__ == '__main__':
         if ref_img is None:
             ref_img = _img
 
-        print("File:", file)
-        _result_strain, _result_center_shift, \
-        _result_images, _outlier = _strain_detector.get_dea_strain([_img], True, True)
-        print("Strain:", _result_strain)
-        print("Area strain:", _result_strain[0, 0])
-        print("Average strain:", _result_strain[0, -1])
-        print("Average approx:", np.mean(_result_strain[0, 1:-1]))
+        for i in range(3):
+            print("File:", file)
+            tic = time.perf_counter()
 
-        cv.imshow("Difference", _result_images[0])
-        cv.waitKey()
+            _result_strain, _result_center_shift, \
+            _result_images, _outlier = _strain_detector.get_dea_strain([_img], True, True)
+            toc = time.perf_counter()
+
+            print("Strain:", _result_strain)
+            print("Area strain:", _result_strain[0, 0])
+            print("Average strain:", _result_strain[0, -1])
+            print("Average approx:", np.mean(_result_strain[0, 1:-1]))
+            print("Elapsed time:", toc - tic, "s")
+
+            cv.imshow("Image", _result_images[0])
+            cv.waitKey()
 
         # print("Outlier: ", _outlier)
         #